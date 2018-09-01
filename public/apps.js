a6cf69324810f1ac358a70eedc4395f48ac87134
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-09-01 15:21:22 -0700

    text is displaying on click

--- public/apps.js
+++ public/apps.js
@@ -1,7 +1,10 @@
 const button = document.getElementById("the-button");
 
 button.addEventListener("click", () => {
-  console.log("hello");
+  const message = "this will be whatever i want";
+  const textBox = document.getElementById("text-box");
+  const text = document.createTextNode(message);
+  textBox.appendChild(text);
 });
 
 function sum(a, b) {

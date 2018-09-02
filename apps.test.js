64a848fd3b0cf62a1a1795663e9d6b16b26fd668
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-09-02 11:47:40 -0700

    paused vanillajs testing

--- apps.test.js
+++ apps.test.js
@@ -1,9 +1,5 @@
-const sum = require("./public/apps").default;
+const sum = require("./public/apps");
 
 test("adds 1 + 2 to equal 3", () => {
-  document.body.innerHTML = "<div>" + '<button id="the-button" />' + "</div>";
-
-  console.log("should have text added", document.body.innerHTML);
-
   expect(sum(1, 2)).toBe(3);
 });
--- public/apps.js
+++ public/apps.js
@@ -1,11 +1,11 @@
-const button = document.getElementById("the-button");
+// const button = document.getElementById("the-button");
 
-button.addEventListener("click", () => {
-  const message = "this will be whatever i want";
-  const textBox = document.getElementById("text-box");
-  const text = document.createTextNode(message);
-  textBox.appendChild(text);
-});
+// button.addEventListener("click", () => {
+//   const message = "this will be whatever i want";
+//   const textBox = document.getElementById("text-box");
+//   const text = document.createTextNode(message);
+//   textBox.appendChild(text);
+// });
 
 function sum(a, b) {
   return a + b;

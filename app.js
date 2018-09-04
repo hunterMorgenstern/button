c4a8dce703fff6d68405f7b905c50a9bfee689bb
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-09-03 17:57:08 -0700

    add procfile and prepare for Heroku deploy

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
-
-const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 

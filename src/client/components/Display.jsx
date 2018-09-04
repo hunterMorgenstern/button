ee4b4e0431061a515b70867190ed7ad7436788f2
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-09-03 23:34:16 -0700

    Added second button for adding quotes and express routing to handle GET

--- src/client/components/Display.jsx
+++ src/client/components/Display.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Display extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "text to display", display: false };
+    this.state = {
+      text: "text to display",
+      display: false,
+      i: 0,
+      quotes: [
+        {
+          quote: "banana man",
+          name: "Francis of Assisi"
+        }
+      ]
+    };
     this.flipText = this.flipText.bind(this);
+    this.newQuote = this.newQuote.bind(this);
+  }
+
+  componentWillMount() {
+    axios
+      .get("/api/quotes")
+      .then(response => {
+        this.setState({
+          text: response.data.quote
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   flipText() {
@@ -13,11 +38,21 @@ class Display extends Component {
     });
   }
 
+  newQuote() {
+    this.setState({
+      text: this.state.quotes[this.state.i].quote,
+      i: this.state.i + 1
+    });
+  }
+
   render() {
     return (
-      <div>
+      <div className="buttons">
         <button onClick={this.flipText} className="main-button">
           {this.state.display ? "Hide text" : "Show Text"}
+        </button>
+        <button onClick={this.newQuote} className="new-quote-button">
+          New Quote
         </button>
         <div className="display-box">{this.state.display ? this.state.text : ""}</div>
       </div>
--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
-
-const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
 
 const app = express();
+const router = express.Router();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "../../public")));
+
+app.use("/api", router);
+
+router.route("/quotes").get((req, res) => {
+  // actually grab data from file and send forward
+  res.json({
+    quote: "Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible.",
+    name: "Francis of Assisi"
+  });
+});
 
 app.get("/", (req, res) => res.send(200));
 
